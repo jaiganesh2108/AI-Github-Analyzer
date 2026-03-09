4a30a44306bd2ba5be3ed690f689fdf81e1937e9
jaiganesh2108 <jaig7335@gmail.com>
2026-03-09 18:20:53 +0530

    initial commit

--- backend/app/gemini_analyzer.py
+++ backend/app/gemini_analyzer.py
@@ -1,2 +1,27 @@
 import google.generativeai as genai
-from config import GEMINI_API_KEY
+from config import GEMINI_API_KEY   
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def analyze_project(readme, structure):
+    prompt = f"""
+    Analyze this Github project.
+    
+    README:
+    {readme}
+    FILE STRUCTURE:
+    {structure}
+
+    Provide:
+    
+    1. Project summary
+    2. Architecture explanation
+    3. Improvement suggestions
+    4. Possible bugs
+    """
+
+    response = model.generate_content(prompt)
+    
+    return response.text
---
+++ backend/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from github_service import parse_repo_url, get_repo_files, get_readme
+from repo_parser import extract_structure
+from gemini_analyzer import analyze_project
+
+app = FastAPI()
+
+@app.get("/analyze")
+
+def analyze(repo_url: str):
+    owner, repo = parse_repo_url(repo_url)
+    files = get_repo_files(owner, repo)
+    readme = get_readme(owner, repo)
+    structure = extract_structure(files)
+    analysis = analyze_project(readme, structure)
+
+    return {
+        "structure": structure,
+        "analysis": analysis
+        }
